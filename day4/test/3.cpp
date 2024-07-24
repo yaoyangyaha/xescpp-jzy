e53cbf22c843bb827f5bfe3ccb870071aa3bb70d
yaoyangyaha <yaoyangyaha@163.com>
2024-07-24 08:59:29 +0800

    update the answer of  day4 exercise 3

--- day4/test/3.cpp
+++ day4/test/3.cpp
@@ -32,18 +32,36 @@ int main(){
 }
 
 */
-bool vis[10000];
-int n,cnt;
-int a[10000][10000];
+#include<iostream>
+using namespace std;
+int a[100][3];
+bool vis[100];
+int n;
+int cnt;
 void dfs(int x){
-    if(x>=n){
+    if(x>n){
         cnt++;
         return;
     }
     for(int i=0;i<2;i++){
         if(vis[a[x][i]]==0){
-            vis[i]=0;
-
+            vis[a[x][i]]=1;
+            dfs(x+1);
+            vis[a[x][i]]=0;
         }
     }
+
 }
+int main(){
+    cin >> n;
+    for(int i=1;i<=n;i++)cin>>a[i][0]>>a[i][1];
+    dfs(1);
+    cout << cnt;
+}
+
+/*
+1
+1        2
+1   2    1    2
+1 2 1 2  1 2  1 2
+*/
