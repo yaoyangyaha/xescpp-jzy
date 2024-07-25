10076c39f7477abc4178d13a7baa2109d52cdbf4
jiapx1024 <123739736+jiapx1024@users.noreply.github.com>
2024-07-25 09:39:41 +0800

    Update main3.cpp by jiapx1024

--- day6/main3.cpp
+++ day6/main3.cpp
@@ -1,1 +1,22 @@
-
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int a[100010],b[100010];
+signed main()
+{
+    int n;
+    cin >> n;
+    int fs=0,zs=0;
+    for (int i=1;i<=n;i++)
+    {
+        cin >> a[i];
+        b[i]=a[i]-a[i-1];
+        if(i>=2)
+        {
+            if(b[i]>0)zs+=b[i];
+            else fs +=abs(b[i]);
+        }
+    }
+    cout << max(zs,fs)<<"\n";
+    cout << abs(zs-fs)+1;
+}
