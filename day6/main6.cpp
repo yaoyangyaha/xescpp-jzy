e6e1236eb9bdcbd4039d9a8d95066a81f9d6a01e
jiapx1024 <123739736+jiapx1024@users.noreply.github.com>
2024-07-25 10:57:24 +0800

    Update main6.cpp

--- day6/main6.cpp
+++ day6/main6.cpp
@@ -3,5 +3,18 @@
 using namespace std;
 int main()
 {
-    freopen("root.in","r",stdin);freopen("root.out","w",stdout);double x;cin >> x;double l=0,r=10;while(r-l>0.000001){double mid=(l+r)/2;double t=mid*mid*mid*mid*mid;if(t<x)l=mid;else r=mid;}cout << l;fclose(stdin);fclose(stdout);return 0;
+    freopen("root.in","r",stdin);
+    freopen("root.out","w",stdout);
+    double x;cin >> x;
+    double l=0,r=10;
+    while(r-l>0.000001)
+    {
+        double mid=(l+r)/2;
+        double t=mid*mid*mid*mid*mid;
+        if(t<x)l=mid;else r=mid;
+    }
+    cout << l;
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
 }
